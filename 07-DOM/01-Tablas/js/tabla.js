3be6535e1c27328eb1a09cd722f88aa29cad8484
paulafc30 <paulafc30@gmail.com>
2024-10-28 12:04:56 +0100

    Clase 28/10

---
+++ 07-DOM/01-Tablas/js/tabla.js
@@ -0,0 +1,56 @@
+let tabla = document.getElementById("generaTabla");
+
+tabla.addEventListener("click", creoTabla);
+
+function creoTabla() {
+  let tam = recogerNum("Dime el tamaño de la matriz: ");
+
+  let seccionTabla = document.getElementById("seccionTabla");
+
+  let nuevaTabla = document.createElement("table");
+
+  let tablaBody = document.createElement("tbody");
+
+  nuevaTabla.style.width = "100%";
+  nuevaTabla.setAttribute("border", 1);
+
+  for (let i = 0; i < tam; i++) {
+    let fila = document.createElement("tr");
+    for (let i = 0; i < tam; i++) {
+      let celda = document.createElement("td");
+      let textoCelda = document.createTextNode(
+        `Fila: ${i + 1}, Columna: ${j + 1}`
+      );
+      celda.appendChild(textoCelda);
+
+      // Añadir un manejador de eventos para cambiar el color al hacer click en la celda oportuna
+      celda.onclick = function () {
+        cambiarColorCelda(this);
+      };
+      fila.appendChild(celda);
+      // añado la celda que acabo de crear a la fila que estoy construyendo
+    }
+    tablaBody.appendChild(fila);
+  }
+  nuevaTabla.appendChild(tablaBody);
+  seccionTabla.appendChild(nuevaTabla);
+}
+
+function cambiarColorCelda(celda){
+    celda.style.backgroundColor = celda.style.backgroundColor === 'blue' ? 'green' : 'blue';
+    
+}
+
+function recogerNum(msj) {
+  let valido = false;
+  let a;
+  do {
+    a = prompt(msj);
+    if (isNaN(a) || a == "") {
+      alert("No valido");
+    } else {
+      valido = true;
+    }
+  } while (valido == false);
+  return Number(a);
+}
--- 07-DOM/JS/dom.js
+++ 07-DOM/JS/dom.js
@@ -1,125 +1,146 @@
-function inicioDOM(){
-    console.log("Dom cargado");
- 
-    document.body.style.background = 'cyan';
- 
-    //document.getElementById
-    let titulo = document.getElementById("title");
-    //objeto en si
-    console.log(titulo);
- 
-    //contenido de la etiqueta 
-    console.log(titulo.innerHTML);
-    console.log(titulo.textContent);
- 
-    //tamaño del contenido de la etiqueta
-    console.log(titulo.textContent.length);
- 
-    //getElementsByName
-    let up_name = document.getElementsByName("up");
-    console.log(up_name[0]);
-    console.log(up_name[1]);
-    console.log(up_name[0].tagName);
-    console.log(up_name[1].tagName);
- 
-    //getElementsByTagName
-    let parrafos = document.getElementsByTagName("p"); //se pone entre () el tipo de etiqueta
-    console.log(parrafos);
-    console.log(`Numero de parrafos que tengo: ${parrafos.length}`);
- 
-    //recorrer y mostrar los parrafos
-    for(let i =0; i <parrafos.length; i++){
-       if(i == 3){
-          parrafos[i].innerHTML = "Parrafo Cambiado";
-       }
-       console.log(`Parrafo ${i+1}: ${parrafos[i].innerHTML}`);
- 
-    }
-    console.log("-----------------------------");
-    //Document.getElementByClassName()
-    parrafos = document.getElementsByClassName("parrafo1");
-    console.log(parrafos)
-    console.log(`Numero de parrafos que tengo: ${parrafos.length}`);
-    for(let i = 0; i <parrafos.length; i++){
-       if(i == 3){
-          parrafos[i].innerHTML = "Parrafo Cambiado";
-       }
-       console.log(`Parrafo ${i+1}: ${parrafos[i].innerHTML}`);
-    }
- 
-    console.log("-----------------------------");
-    //querySelector() nos devuelve la primera ocurrencia = que el elemento del ElementById
-    let parrafo1 = document.querySelector("p.parrafo1");
-    console.log(parrafo1);
-    let titulos = document.getElementById("parrafo1");
-    console.log(titulos);
- 
-    parrafo1 = document.querySelector(".projects p");
-    console.log(parrafo1);
- 
-    parrafo1 = document.querySelector("projects");
- 
-    //querySelectorAll() array vacio si no encuentro nada array con cosas si si
-    let elementos = document.querySelectorAll("p.parrafo1");
-    for(let i = 0; i <parrafos.length; i++){
-       console.log(`Elemento ${i+1}: ${parrafos[i].innerHTML}`);
-    }
-    
-    //Selecciono el DOM parcialmente
-    let sectionProjects = document.getElementById('proyectos');
- 
-    console.log(sectionProjects);
-    elementos = sectionProjects.querySelectorAll('p.parrafos1'); 
-    console.log(`Número de elementos que tengo: ${elementos.length}`);
-    for (let i=0; i<elementos.length; i++){
-       console.log(`Elemento ${i+1}: ${elementos[i].innerHTML}`);
-    }
-          
- }
- 
- let estado = false;
- function color_cabecera(cabecera){
-    // OPCION 1
-    // if(estado){
-    //    cabecera.style.color = 'red';
-    //    estado = false;
-    // }else{
-    //    cabecera.style.color = 'black';
-    //    estado = true;
-    // }
- 
-    // OPCION 2 
-    cabecera.style.color = (cabecera.style.color === 'red') ? 'black' : 'red';
- }
- 
- 
- //Vamos a modificar el titulo 2 
- function color_subtitulo(cabecera){
-    cabecera.style.color = (cabecera.style.color === 'pink') ? 'pink' : 'orange';
- 
- }
- 
- function color_subtitulo2(){
-    let elemento = document.getElementById('subtitle');
-    elemento.style.color = (elemento.style.color === 'orange') ? 'pink' : 'orange';
- 
- }
- 
- let elemento = document.getElementById('subtitle');
- elemento.style.color = 'orange';
- document.getElementById('subtitle').onclick = color_subtitulo2;
- 
- //opcion 1 llamada a eventos desde JS
- // elemento.onclick = color_subtitulo(elemento);
+function inicioDOM() {
+  console.log("Dom cargado");
 
- let titleh3 = document.getElementById('H3title');
- titleh3.addEventListener('click', cambioSubtitulo3());
+  document.body.style.background = "cyan";
 
-elemento = document.getElementById('H3title');
-elemento.style.color = 'blue';
+  //document.getElementById
+  let titulo = document.getElementById("title");
+  //objeto en si
+  console.log(titulo);
 
- function color_subtitulo3(){
-    let elemento = document.getElementById('H3title');
-    elemento.style.color = (elemento.style.color === 'orange') ? 'pink' : 'orange';
-    
- }
+  //contenido de la etiqueta
+  console.log(titulo.innerHTML);
+  console.log(titulo.textContent);
+
+  //tamaño del contenido de la etiqueta
+  console.log(titulo.textContent.length);
+
+  //getElementsByName
+  let up_name = document.getElementsByName("up");
+  console.log(up_name[0]);
+  console.log(up_name[1]);
+  console.log(up_name[0].tagName);
+  console.log(up_name[1].tagName);
+
+  //getElementsByTagName
+  let parrafos = document.getElementsByTagName("p"); //se pone entre () el tipo de etiqueta
+  console.log(parrafos);
+  console.log(`Numero de parrafos que tengo: ${parrafos.length}`);
+
+  //recorrer y mostrar los parrafos
+  for (let i = 0; i < parrafos.length; i++) {
+    if (i == 3) {
+      parrafos[i].innerHTML = "Parrafo Cambiado";
+    }
+    console.log(`Parrafo ${i + 1}: ${parrafos[i].innerHTML}`);
+  }
+  console.log("-----------------------------");
+  //Document.getElementByClassName()
+  parrafos = document.getElementsByClassName("parrafo1");
+  console.log(parrafos);
+  console.log(`Numero de parrafos que tengo: ${parrafos.length}`);
+  for (let i = 0; i < parrafos.length; i++) {
+    if (i == 3) {
+      parrafos[i].innerHTML = "Parrafo Cambiado";
+    }
+    console.log(`Parrafo ${i + 1}: ${parrafos[i].innerHTML}`);
+  }
+
+  console.log("-----------------------------");
+  //querySelector() nos devuelve la primera ocurrencia = que el elemento del ElementById
+  let parrafo1 = document.querySelector("p.parrafo1");
+  console.log(parrafo1);
+  let titulos = document.getElementById("parrafo1");
+  console.log(titulos);
+
+  parrafo1 = document.querySelector(".projects p");
+  console.log(parrafo1);
+
+  parrafo1 = document.querySelector("projects");
+
+  //querySelectorAll() array vacio si no encuentro nada array con cosas si si
+  let elementos = document.querySelectorAll("p.parrafo1");
+  for (let i = 0; i < parrafos.length; i++) {
+    console.log(`Elemento ${i + 1}: ${parrafos[i].innerHTML}`);
+  }
+
+  //Selecciono el DOM parcialmente
+  let sectionProjects = document.getElementById("proyectos");
+
+  console.log(sectionProjects);
+  elementos = sectionProjects.querySelectorAll("p.parrafos1");
+  console.log(`Número de elementos que tengo: ${elementos.length}`);
+  for (let i = 0; i < elementos.length; i++) {
+    console.log(`Elemento ${i + 1}: ${elementos[i].innerHTML}`);
+  }
+}
+
+let estado = false;
+function color_cabecera(cabecera) {
+  // OPCION 1
+  // if(estado){
+  //    cabecera.style.color = 'red';
+  //    estado = false;
+  // }else{
+  //    cabecera.style.color = 'black';
+  //    estado = true;
+  // }
+
+  // OPCION 2
+  cabecera.style.color = cabecera.style.color === "red" ? "black" : "red";
+}
+
+//Vamos a modificar el titulo 2
+function color_subtitulo(cabecera) {
+  cabecera.style.color = cabecera.style.color === "pink" ? "pink" : "orange";
+}
+
+function color_subtitulo2() {
+  let elemento = document.getElementById("subtitle");
+  elemento.style.color = elemento.style.color === "orange" ? "pink" : "orange";
+}
+
+let elemento = document.getElementById("subtitle");
+elemento.style.color = "orange";
+document.getElementById("subtitle").onclick = color_subtitulo2;
+
+//opcion 1 llamada a eventos desde JS
+// elemento.onclick = color_subtitulo(elemento);
+
+elemento = document.getElementById("H3title");
+elemento.style.color = "green";
+H3title.addEventListener("click", color_subtitulo3);
+H3title.addEventListener("click", color_subtitulo3);
+
+function color_subtitulo3() {
+  let elemento = document.getElementById("H3title");
+  elemento.style.color = elemento.style.color === "green" ? "orange" : "green";
+}
+
+let c1 = document.getElementById("caja1");
+c1.addEventListener("click", cambioClase1);
+
+let c2 = document.getElementById("caja2");
+c2.addEventListener("click", cambioClase2);
+
+function cambioClase1() {
+  // c1.style.background = 'black';
+  // c1.style.color = 'white';
+  //classList nos da una lista de las clases que tiene el elemento
+  //genera un array, nos permite mirar si la contiene.
+  if (c1.classList.contains("caja--negra")) {
+    c1.classList.remove("caja--negra");
+    c1.classList.add("caja--roja");
+  } else {
+    c1.classList.remove("caja--roja");
+    c1.classList.add("caja--negra");
+  }
+}
+function cambioClase2() {
+  if (c2.classList.contains("caja--negra")) {
+    c2.classList.remove("caja--negra");
+    c2.classList.add("caja--roja");
+  } else {
+    c2.classList.replace("caja--roja", "caja--negra");
+  }
+}
